JorgeFerrer/liferay-portal

LPS-66238 Rename file

modules/apps/foundation/users-admin/users-admin-web/src/main/resources/META-INF/resources/view_tree.jsp
@@ -288,7 +288,7 @@ if (organization != null) {
 					}
 					%>
 
-					<%@ include file="/organization/organization_columns.jspf" %>
+					<%@ include file="/organization/organization_member_search_columns.jspf" %>
 				</liferay-ui:search-container-row>
 
 				<liferay-ui:search-iterator markupView="lexicon" resultRowSplitter="<%= new OrganizationResultRowSplitter() %>" searchContainer="<%= membersSearchContainer %>" />

Halelin/com-liferay-users-admin

users-admin-web/src/main/resources/META-INF/resources/view_tree.jsp
@@ -288,7 +288,7 @@ if (organization != null) {
 					}
 					%>
 
-					<%@ include file="/organization/organization_columns.jspf" %>
+					<%@ include file="/organization/organization_user_search_columns.jspf" %>
 				</liferay-ui:search-container-row>
 
 				<liferay-ui:search-iterator markupView="lexicon" resultRowSplitter="<%= new OrganizationResultRowSplitter() %>" searchContainer="<%= membersSearchContainer %>" />