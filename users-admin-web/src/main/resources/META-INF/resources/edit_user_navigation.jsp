drewbrokke/liferay-portal

LPS-76537 Use constants for attribute names

modules/apps/foundation/users-admin/users-admin-web/src/main/resources/META-INF/resources/edit_user_navigation.jsp
@@ -17,12 +17,12 @@
 <%@ include file="/init.jsp" %>
 
 <%
-String actionCommandName = (String)request.getAttribute("actionCommandName");
-String jspPath = (String)request.getAttribute("jspPath");
+String actionCommandName = (String)request.getAttribute(UserScreenNavigationWebKeys.ACTION_COMMAND_NAME);
+String jspPath = (String)request.getAttribute(UserScreenNavigationWebKeys.JSP_PATH);
 
 User selUser = PortalUtil.getSelectedUser(request);
 
-request.setAttribute("user.selUser", selUser);
+request.setAttribute(UserScreenNavigationWebKeys.SELECTED_USER, selUser);
 %>
 
 <portlet:actionURL name="<%= actionCommandName %>" var="actionCommandURL" />

modules/apps/foundation/users-admin/users-admin-web/src/main/resources/META-INF/resources/user/announcements.jsp
@@ -17,7 +17,7 @@
 <%@ include file="/init.jsp" %>
 
 <%
-User selUser = (User)request.getAttribute("user.selUser");
+User selUser = (User)request.getAttribute(UserScreenNavigationWebKeys.SELECTED_USER);
 
 List<AnnouncementsDelivery> deliveries = null;
 

Halelin/com-liferay-users-admin

users-admin-web/src/main/resources/META-INF/resources/edit_user_navigation.jsp
@@ -17,12 +17,12 @@
 <%@ include file="/init.jsp" %>
 
 <%
-String actionCommandName = (String)request.getAttribute("actionCommandName");
-String jspPath = (String)request.getAttribute("jspPath");
+String actionCommandName = (String)request.getAttribute(UsersAdminWebKeys.ACTION_COMMAND_NAME);
+String jspPath = (String)request.getAttribute(UsersAdminWebKeys.JSP_PATH);
 
 User selUser = PortalUtil.getSelectedUser(request);
 
-request.setAttribute("user.selUser", selUser);
+request.setAttribute(UsersAdminWebKeys.SELECTED_USER, selUser);
 %>
 
 <portlet:actionURL name="<%= actionCommandName %>" var="actionCommandURL" />

users-admin-web/src/main/resources/META-INF/resources/user/announcements.jsp
@@ -17,7 +17,7 @@
 <%@ include file="/init.jsp" %>
 
 <%
-User selUser = (User)request.getAttribute("user.selUser");
+User selUser = (User)request.getAttribute(UsersAdminWebKeys.SELECTED_USER);
 
 List<AnnouncementsDelivery> deliveries = null;
 